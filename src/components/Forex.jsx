bfee773abfbc99dca50b2bf4a56fa37ef681a5a0
YiPeng Teo <yipengteo@yahoo.com.sg>
2022-01-23 20:37:21 +0800

    attempted to create cards for major pairs

--- src/components/Forex.jsx
+++ src/components/Forex.jsx
@@ -6,16 +6,32 @@ const Forex = () => {
     const symbol = 'USD/JPY';
     const { data: fxpairs, isFetching } = useGetPairsListQuery();
     const { data: fxrates } = useGetExchangeRateQuery(symbol); 
+    const major = ['EUR/USD', 'USD/JPY', 'GBP/USD', 'USD/CHF', 'AUD/USD', 'USD/CAD', 'NZD/USD'];
     if(isFetching) return "Loading...";
     console.log(fxpairs);
     console.log(fxpairs.data[1990]);
     console.log(fxrates);
-    const allPairs = []; 
+    const allPairs = []; // dictionary to hold all pairs and their price
 
     return (
-        <div className="bg-black h-screen">
-            hello theer
-        </div>
+        <>
+            <div className="bg-black h-screen text-amber-300 flex flex-col justify-center items-evenly">
+                <div>
+                    <h1>Major Currency Pairs</h1>
+                    <div>
+                        {major.map((pair) => (
+                            <div className="max-w-sm rounded overflow-hidden shadhow-lg" key={pair}>
+                                <p>{pair}</p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div>
+                    <h1>Other Pairs</h1>
+                </div>
+            </div>
+        </>
+        
     );
 };
 
