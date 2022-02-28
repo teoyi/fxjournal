7af5a239f5a72f1d254e7bd815618aaf39b2cf5e
YiPeng Teo <yipengteo@yahoo.com.sg>
2022-02-28 16:34:03 +0800

    connected trading journal link to display all available journals for selection on side bar

--- client/src/components/Dashboard/SideNav.jsx
+++ client/src/components/Dashboard/SideNav.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 import useLogout from '../../hooks/useLogout';
+// import axios from '../../api/axios';
+import useAxiosPrivate from '../../hooks/useAxiosPrivate';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+
+
 import { TiHome } from "react-icons/ti";
 import { AiOutlineLineChart } from "react-icons/ai";
 import { IoNewspaperOutline } from "react-icons/io5";
 import { IoIosJournal } from "react-icons/io";
 import { RiSettings5Fill, RiLogoutBoxRLine } from "react-icons/ri";
-import { BiLogOut } from "react-icons/bi";
+
+
+const ALL_JOURNAL_URL = 'journals/all';
+
 const SideNav = () => {
+    const [journals, setJournals] = useState([]);
+    const [showList, setShowList] = useState(false);
+    const axiosPrivate = useAxiosPrivate();
     const navigate = useNavigate(); 
+    const location = useLocation();
     const logout = useLogout();
     /// logout handler 
     const logoutHandler = async () => {
@@ -18,6 +29,42 @@ const SideNav = () => {
         navigate('/');
     };
 
+    useEffect(() => {
+        let isMounted = true;
+        const controller = new AbortController();
+
+        const getJournals = async () => {
+            try {
+                const response = await axiosPrivate.get(ALL_JOURNAL_URL, {
+                    signal: controller.signal
+                });
+
+                const data = response?.data; 
+                const list = []; 
+
+                if (data.length > 0) {
+                    data.forEach(journalObj => { 
+                        list.push(journalObj.journalName);
+                    });
+                };
+                isMounted && setJournals(list);
+            } catch (err) {
+                console.error(err);
+                navigate('/login', { state: { from: location }, replace: true });
+            }
+        }
+
+        getJournals();
+
+        return () => {
+            isMounted = false;
+            controller.abort();
+        }
+    }, [])
+
+    const handleShowList = () => {
+        setShowList(prev => !prev);
+    }
 
     return (
     <div className="w-1/6 h-screen font-semibold flex flex-col justify-start bg-banana text-black">
@@ -29,7 +76,13 @@ const SideNav = () => {
                 <Link className="my-5 flex flex-row items-center" to="/dashboard"><TiHome className="text-2xl mr-3 ml-8"/>Dashboard</Link>
                 <Link className="mb-5 flex flex-row items-center" to="/dashboard/forex"><AiOutlineLineChart className="text-2xl mr-3 ml-8"/>Charts</Link>
                 <Link className="mb-5 flex flex-row items-center" to="/dashboard/news"><IoNewspaperOutline className="text-2xl mr-3 ml-8"/>News</Link>
-                <Link className="mb-5 flex flex-row items-center" to="/dashboard/journals"><IoIosJournal className="text-2xl mr-3 ml-8"/>Trading Journal</Link>
+                <Link className="mb-2 flex flex-row items-center" to="/dashboard/journals" onClick={handleShowList}><IoIosJournal className="text-2xl mr-3 ml-8"/>Trading Journal</Link>
+                <div className={`w-full flex flex-col transition-all ${showList? 'block' : 'hidden'}`}>
+                    {journals.map((title) => (
+                        <Link className='ml-12 mr-5 mb-2 text-sm whitespace-nowrap text-ellipsis overflow-hidden border-b border-black' to={`/dashboard/journals/${title}`}>{title}</Link>
+                    ))}
+                </div>
+                
             </div>
             <div className="flex flex-col">
                 <Link className="mb-5 flex flex-row items-center" to="/dashboard/settings"><RiSettings5Fill className="text-2xl mr-3 ml-8"/>Settings</Link>
