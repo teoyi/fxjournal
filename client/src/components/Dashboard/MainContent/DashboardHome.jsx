84e0ad756110e762c6774b6f8466d393c0ad285a
YiPeng Teo <yipengteo@yahoo.com.sg>
2022-03-11 15:51:11 +0800

    Changed dashboard display, connected journals information. Need to display journal history and recent journal entries

--- client/src/components/Dashboard/MainContent/DashboardHome.jsx
+++ client/src/components/Dashboard/MainContent/DashboardHome.jsx
@@ -1,34 +1,90 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import useAxiosPrivate from '../../../hooks/useAxiosPrivate';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
+
+
+const JOURNAL_BALANCE_URL = 'journals/getBalance';
+const JOURNAL_HISTORY_URL = 'journals/getBalanceHistory';
+
 
 const DashboardHome = () => {
+  const axiosPrivate = useAxiosPrivate();
+  const [cookies, setCookie] = useCookies(['currentJournal']);
+  const [journalBalance, setJournalBalance] = useState('0');
+  const [journalHistory, setJournalHistory] = useState([]);
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    let isMounted = true;
+    const controller = new AbortController();
+
+    const getJournals = async () => {
+      try {
+        const resHistory = await axiosPrivate.post(
+          JOURNAL_HISTORY_URL,
+          JSON.stringify({ "id": cookies.currentJournal })
+        );
+
+        const historyData = resHistory?.data;
+        setJournalHistory(historyData);
+
+        const resBalance = await axiosPrivate.post(
+          JOURNAL_BALANCE_URL,
+          JSON.stringify({ "id": cookies.currentJournal }),
+          { signal: controller.signal }
+        );
+
+        console.log(resBalance?.data)
+        const data = resBalance?.data;
+
+        isMounted && setJournalBalance(data);
+      } catch (err) {
+        console.error(err);
+        navigate('/login', { state: { from: location }, replace: true });
+      }
+    }
+
+    getJournals();
+
+    return () => {
+      isMounted = false;
+      controller.abort();
+    }
+  }, []);
   return (
-    <div className="flex flex-col h-dash-height overflow-y-auto py-3 px-5">
-      <div className="flex flex-row h-3/5">
-        <div className='w-2/5 p-5'>ACCOUNT STATS</div>
-        <div className='h-full w-3/5 bg-banana text-black rounded-3xl p-5'>ACCOUNT GRAPH</div>
+    <div className="flex flex-col overflow-y-auto py-3 px-5">
+      <div className="flex flex-row h-[350px] w-full p-5 ">
+        <div className='w-2/5 p-5 mr-5 bg-banana text-black rounded-3xl font-semibold text-lg'>ACCOUNT STATS</div>
+        <div className='w-3/5 bg-banana text-black rounded-3xl p-5 font-semibold text-lg'>ACCOUNT GRAPH</div>
       </div>
-      <div className='w-full flex flex-row items center p-5'>
-        <div className="mr-5 flex flex-col w-4/5 bg-banana text-black rounded-2xl p-5">
-          <h1 className='text-lg border-b border-black font-semibold'>Strategy</h1>
-          <div className='flex flex-row mt-2'>
-            <div className='w-1/2 h-[100px] text-ellipsis overflow-hidden border border-black m-2 cursor-pointer rounded-2xl p-2'>
-              Entry: <br /> 1. <br /> 2. <br /> 3. <br /> 4. <br />
-            </div>
-            <div className='w-1/2 h-[100px] text-ellipsis overflow-hidden border border-black m-2 cursor-pointer rounded-2xl p-2'>
-              Exit:  <br /> 1. <br /> 2. <br /> 3. <br /> 4. <br />
-            </div>
-          </div>
-        </div>
-        <div className='w-1/5 border-2 border-banana bg-banana text-black rounded-2xl p-5'>
+      <div className='w-full flex flex-row items-center  justify-between p-5 h-[230px]'>
+        <div className='w-1/4 border-2 border-banana bg-banana text-black rounded-2xl p-5 h-full mr-3'>
           <h1 className='text-lg font-semibold'>Profitability</h1>
           <div className='text-3xl text-center h-journal-percent flex justify-center items-center'>64%</div>
         </div>
+        <div className='w-1/4 border-2 border-banana bg-banana text-black rounded-2xl p-5 h-full mr-3'>
+          <h1 className='text-lg font-semibold'>Position Summary</h1>
+          <div className='text-3xl text-center h-journal-percent flex justify-center items-center'>64%</div>
+        </div>
+        <div className='w-1/4 border-2 border-banana bg-banana text-black rounded-2xl p-5 h-full  mr-3'>
+          <h1 className='text-lg font-semibold'>Currency Summary</h1>
+          <div className='text-3xl text-center h-journal-percent flex justify-center items-center'>64%</div>
+        </div>
+        <div className='w-1/4 border-2 border-banana bg-banana text-black rounded-2xl p-5 h-full mr-3'>
+          <h1 className='text-lg font-semibold'>Account Balance</h1>
+          <div className='text-3xl text-center h-journal-percent flex justify-center items-center'>${journalBalance}</div>
+        </div>
         {/* <button className="bg-banana text-black px-3 py-1 rounded-full">+ Entry</button> */}
       </div>
-      <div className="h-2/5 p-5">
-        Recent journal entries
+      <div className="h-[200px] rounded-2xl bg-banana text-black m-5">
+        <div className='p-5 font-semibold text-lg'>
+          Recent journal entries
+        </div>
       </div>
-    </div>
+    </div >
   )
 }
 
